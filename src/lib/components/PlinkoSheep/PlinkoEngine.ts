bbc61cd3b0eed18dac81709450cb907d625159a3
wparke4 <willparke4@gmail.com>
2025-08-02 13:45:29 -0500

    spacebar to reset after dying

--- src/lib/components/PlinkoSheep/PlinkoEngine.ts
+++ src/lib/components/PlinkoSheep/PlinkoEngine.ts
@@ -100,8 +100,8 @@ export default class PlinkoEngine {
       if (event.code === 'Space' && !event.repeat) {
         event.preventDefault(); // Prevent page scrolling
         
-        // If cash out is complete OR celebrating, reset for new game
-        if (this.isCashOutComplete || this.isCashOutCelebrating) {
+        // If cash out is complete/celebrating OR game is dead, reset for new game
+        if (this.isCashOutComplete || this.isCashOutCelebrating || this.isGameDead) {
           this.resetGame();
         }
         // If game is in progress, cash out. Otherwise, drop a ball.
